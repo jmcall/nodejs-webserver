f950a5ad2609dace3300278170e0b35c63b00a31
John McAllister <john@Johns-Mac-Studio.local>
2022-04-15 13:13:33 -0600

    Added HTML Rendering

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
 
 const server = http.createServer(function(req,res) {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
 
-res.write('Hello Node')
-
-res.end()
-
+            res.writeHead(404)
+        
+            res.write('Error: File Not Found')
+        
+        } else {
+        
+            res.write(data)
+        
+        }
+    
+        res.end()
+    })
 })
 
 server.listen(port, function(error) {
 
-if (error) {
+    if (error) {
 
-console.log('something went wrong:', error)
+        console.log('something went wrong:', error)
 
-} else {
+    } else {
 
-console.log('Server is listening on port: ' + port)
+        console.log('Server is listening on port: ' + port)
 
-}
+    }
 
 })
